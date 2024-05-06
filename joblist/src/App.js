c7acd94995ab7317e401f83ea85259cdcdd3b80f
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2024-05-06 14:59:45 +0530

    removed unused lines of code

--- joblist/src/App.js
+++ joblist/src/App.js
@@ -1,15 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
 
-import Home from "./containers/Home.js";
-import NotFound from "./containers/NotFound.js";
+import Home from "./containers/Home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
   );
